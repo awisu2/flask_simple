46431eddc33d4640574812bf555e4cbf9c19521c
awisu2 <awisu2@gmail.com>
2019-06-07 17:26:59 +0900

    [modify] dbのロード処理を追加

--- project/database.py
+++ project/database.py
@@ -1,8 +1,11 @@
+from project.models import importDbModels
+
+
 def init_db(app, db):
     db.init_app(app)
-    import project.models
 
     # migrate setting
     if app.config.get('MIGRADE'):
+        importDbModels()
         from flask_migrate import Migrate
         Migrate(app, db)
